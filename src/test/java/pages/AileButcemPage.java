35209fe8623909ae2973bbf67359577fc7d2f15e
Elif Kesen <elifkesen@gmail.com>
2024-11-29 20:25:59 +0100

    29.11 v2

---
+++ src/test/java/pages/AileButcemPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class AileButcemPage {
+    ////*[@text='Giriş Yap']
+
+    public AileButcemPage(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getAndroidDriver(),Duration.ofSeconds(15)),this);}
+
+    public void ilkekranAyarlari() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            Thread.sleep(2000);
+            ReusableMethods.ekranKaydirmaMethodu(930,1200,500,130,1200);
+        }
+        ReusableMethods.scrollWithUiScrollableAndClick("Giriş Yap");
+    }
+
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    public MobileElement emailKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    public MobileElement sifreKutusu;
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/resources/Features/aileButcem.feature
@@ -0,0 +1,10 @@
+Feature: Kullanici Aile Butcem uygulamasindaki gorevleri yerine getirir
+  @aile
+  Scenario:Kullanici Aile Butcem uygulamasinda profil bilgilerinin degistirir
+    * ilk ekran ayarlarini yapin ve ardindan login sayfasina ulasin
+    * "mail" ve "password" bilgilerini girerek kullanici bilgileriyle "Giriş Yap"
+    * uygulamaya kullanici bilgileriyle giris yapildigini dogrulayin
+    * sol kisimdaki menuden "Hesabım" bolumune gidin
+    * hesabim sayfasindaki bilgileri degistirerek "isim" "soyisim" "sehir" "yas" "meslek" ve cinsiyet "Kadın" degisikleri kaydedin ve değişikleri dogrulayin
+    * uygulamayi kapatir
+
