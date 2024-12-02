0a6efcc21f5e24b8fb4f284051280a49ab0517fe
Elif Kesen <elifkesen@gmail.com>
2024-12-02 19:26:56 +0100

    02.12

--- src/test/java/stepDefinitions/BrowserStepDef.java
+++ src/test/java/stepDefinitions/BrowserStepDef.java
@@ -1,7 +1,10 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import utilities.BrowserDriver;
+import utilities.ConfigReader;
 
 public class BrowserStepDef {
     @Given("Kullanici hepsiburada adresine gider")
@@ -9,4 +12,33 @@ public class BrowserStepDef {
         BrowserDriver.getBrowserDriver().get("https://www.hepsiburada.com");
 
     }
+    @Given("{string} sayfasina gider")
+    public void sayfasina_gider(String url) {
+        BrowserDriver.getBrowserDriver().get(ConfigReader.getProperty(url));
+
+
+    }
+    @Then("Avantajli urunler icon u oldugunu dogrular")
+    public void avantajli_urunler_icon_u_oldugunu_dogrular() throws InterruptedException {
+
+       // BrowserDriver.getBrowserDriver().findElementByXPath("(//*[@src='https://images.hepsiburada.net/banners/s/1/104-104/IMAGE-BADGE-EnAvantajli133709686739364322.png'])[1]").isDisplayed();
+        Assert.assertTrue("Icon",BrowserDriver.getBrowserDriver().findElementByXPath("(//*[@src='https://images.hepsiburada.net/banners/s/1/104-104/IMAGE-BADGE-EnAvantajli133709686739364322.png'])[1]").isDisplayed());
+        Thread.sleep(2000);
+
+
+
+    }
+    @Then("Avantajli urunler listesinin acildigini dogrular")
+    public void avantajli_urunler_listesinin_acildigini_dogrular() {
+
+
+    }
+
+
+
+
+
+
+
 }
+
